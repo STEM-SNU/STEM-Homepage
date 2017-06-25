df750ba068acda15ca8d0558552d9086224edc36
jun9303 <jun9303@gmail.com>
2017-06-25 17:04:58 +0900

    [20170625] Major Updates

--- app/static/adminLTE/dist/js/member.js
+++
@@ -1,7 +0,0 @@
-jQuery("[class^=member-card]").easyModal({
-	overlay:0.2
-});
-
-function closeModal() {
-	jQuery("[class^=member-card]").trigger("closeModal");
-}
--- run.py
+++
@@ -1,7 +0,0 @@
-#-*-coding:utf-8-*-
-#!venv/bin/python
-from app import app
-
-if __name__ == '__main__':
-    app.debug = True  # change before commit
-    app.run(host='0.0.0.0',port=80, use_reloader=False)
